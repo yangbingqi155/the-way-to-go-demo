ec0e1adcc9579d5bb4ad99dddb2db2e818e8d9f1
yangbinqi <ybq@outlook.com>
2019-12-05 16:49:45 +0800

    goroutine

---
+++ chapter1427/goroutineNotice.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//Empty empty interface for sem
+type Empty interface{}
+
+var empty Empty
+
+//N data length
+const N = 50
+
+func main() {
+	fmt.Println("goroutine notice")
+	data := make([]float64, N)
+	res := make([]float64, N)
+	sem := make(chan Empty, N)
+	for i := 0; i < N; i++ {
+		data[i] = float64(i)
+	}
+	for i, ix := range data {
+		go func(i int, ix float64) {
+			time.Sleep(2 * 1e9)
+			res[i] = ix * float64(5)
+			sem <- empty
+		}(i, ix)
+	}
+	for i := 0; i < N; i++ {
+		<-sem
+	}
+	for i := 0; i < N; i++ {
+		fmt.Printf("Index:%d,Value:%f\n", i, res[i])
+	}
+}
